253560252fd9f9c31733a005524447386080ce3a
huzhihui <huzhihui@hztianque.com>
2019-11-14 13:52:01 +0800

    便民服务相关修改

--- light_video/app/__init__.py
+++ light_video/app/__init__.py
@@ -4,7 +4,7 @@ from flask import (Flask, render_template,url_for,redirect,Blueprint,request,fla
 import pymysql,os
 
 
-app = Flask(__name__)
+app = Flask(__name__,static_url_path='/movieStatic')
 app.config[
     'SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:0shiwodeweiyi@106.12.180.188:3306/movie?charset=utf8mb4"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
@@ -24,25 +24,25 @@ app.register_blueprint(home_blueprint,url_prefix='/movie')
 app.register_blueprint(admin_blueprint, url_prefix='/movie/admin')
 app.register_blueprint(mobile_blueprint,url_prefix='/mobile')
 
-# @app.context_processor
-# def inject_url():
-#     data = {
-#         "url_for": dated_url_for,
-#     }
-#     return data
+@app.context_processor
+def inject_url():
+    data = {
+        "url_for": dated_url_for,
+    }
+    return data
 
-# def dated_url_for(endpoint, **values):
-#     print(endpoint,values)
-#     if endpoint == 'movieStatic':
-#         filename = values.get('filename', None)
-#         if filename:
-#             endpoint = 'static'
-#             file_path = os.path.join(app.root_path, endpoint, filename)
-#             print(file_path)
-#             values['v'] = int(os.stat(file_path).st_mtime)  # 取文件最后修改时间的时间戳，文件不更新，则可用缓存
-#             return url_for(endpoint, **values)
-#     else:
-#         return url_for(endpoint,**values)
+def dated_url_for(endpoint, **values):
+    print(endpoint,values)
+    if endpoint == 'movieStatic':
+        filename = values.get('filename', None)
+        if filename:
+            endpoint = 'static'
+            file_path = os.path.join(app.root_path, endpoint, filename)
+            print(file_path)
+            values['v'] = int(os.stat(file_path).st_mtime)  # 取文件最后修改时间的时间戳，文件不更新，则可用缓存
+            return url_for(endpoint, **values)
+    else:
+        return url_for(endpoint,**values)
 
 @app.errorhandler(404)
 def page_not_fount(error):
